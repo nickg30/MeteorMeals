82bf370769013ff66af7f245b827fbd3a994db20
Nicholas Guerra <nick@scriptthread.com>
2017-08-26 12:14:37 -0700

    first commit

---
+++ mobile-config.js
@@ -0,0 +1,9 @@
+App.icons({
+    'iphone6': 'icons/icon-60.png',
+    'iphone_2x': 'icons/icon-60@2x.png',
+    // More screen sizes and platforms...
+  });
+  App.launchScreens({
+    'iphone': 'splash/Default~iphone.png',
+    // More screen sizes and platforms...
+  });
